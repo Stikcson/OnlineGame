520b92c57ce01bd3bdd79c36cac3be785b8a78b6
Stikcson <stikcson2000@mail.ru>
2020-06-21 16:38:18 +0500

    Первая версия -Чат -Камера

---
+++ Assets/Scripts/PickUpCoin.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUpCoin : MonoBehaviour
+{
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.transform.tag == "Player")
+        {
+            gameObject.SetActive(false);
+            Invoke("Respawn", 3);
+        }
+
+    }
+    void Respawn()
+        {
+            gameObject.SetActive(true);
+        }
+}
